5854586b349b1cac4645ddd8e0e42998b8cf94f7
nogayama <NOGAYAMA@jp.ibm.com>
2025-09-22 00:39:48 +0900

    feat: hello picocli
    Signed-off-by: nogayama <NOGAYAMA@jp.ibm.com>

--- src/main/java/mypkg/App.java
+++ src/main/java/mypkg/App.java
@@ -1,7 +1,23 @@
 package mypkg;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "hello", description = "Say hello.", //
+		mixinStandardHelpOptions = true)
+public class App implements Runnable {
+
+	@Option(names = { "-n", "--name" }, defaultValue = "world", description = "Name to greet")
+	protected String name;
+
+	@Override
+	public void run() {
+		System.out.println("Hello " + this.name);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		int exitCode = new CommandLine(new App()).execute(args);
+		System.exit(exitCode);
 	}
 }
